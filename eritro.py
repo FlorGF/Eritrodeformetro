04432f5478fb19be7133562376575d82a5ca5235
Florencia <florgomezfava.97@gmail.com>
2023-01-18 18:53:38 -0300

    progreso

--- eritro.py
+++ eritro.py
@@ -7,6 +7,8 @@ root=tk.Tk()
 root.title("Eritrodeformetro")
 root.config(width=400,height=300)
 #defino los botones
+
+#botones de ensayo
 label_ensayo=ttk.LabelFrame(root,text="Ensayos:")
 label_ensayo.grid(column=0,row=0)
 bot_carga=ttk.Button(label_ensayo,text="Carga")
@@ -15,10 +17,14 @@ bot_descarga=ttk.Button(label_ensayo,text="Descarga")
 bot_descarga.grid(column=0,row=2)
 bot_dinamico=ttk.Button(label_ensayo,text="Dinamico")
 bot_dinamico.grid(column=0,row=3)
+
+#botones de motor
 bot_encender=ttk.Button(root,text="Encender motor")
 bot_encender.grid(column=1,row=0)
 bot_apagar=ttk.Button(root,text="Apagar motor")
 bot_apagar.grid(column=1,row=1)
+
+#botones del puerto
 label_puerto=ttk.LabelFrame(root,text="Puerto:")
 label_puerto.grid(column=1,row=2)
 abrir_puerto=ttk.Radiobutton(label_puerto,text="Abrir")
@@ -26,7 +32,24 @@ abrir_puerto.grid(column=1,row=3)
 cerrar_puerto=ttk.Radiobutton(label_puerto,text="Cerrar")
 cerrar_puerto.grid(column=1,row=4)
 
+#botones de frecuencia
+label_frecuencia=ttk.LabelFrame(root,text="Frecuencia:")
+label_puerto.grid(column=2,row=0)
+cinco=ttk.Radiobutton(label_frecuencia,text="0,5Hz")
+cinco.grid(column=2,row=1)
+#diez=ttk.Radiobutton(label_frecuencia,text="1Hz")
+#diez.grid(column=0,row=7)
+#quince=ttk.Radiobutton(label_ensayo,text="1,5Hz")
+#quince.grid(column=0,row=8)
 
 
-
+#botones de frecuencia
+#label_velocidad=ttk.LabelFrame(root,text="Velocidad:")
+#label_velocidad.grid(column=1,row=5)
+#menor=ttk.Radiobutton(label_velocidad,text="17rpm")
+#menor.grid(column=1,row=6)
+#intermedia=ttk.Radiobutton(label_velocidad,text="35rpm")
+#intermedia.grid(column=1,row=7)
+#mayor=ttk.Radiobutton(label_velocidad,text="70rpm")
+#mayor.grid(column=1,row=8)
 root.mainloop()
