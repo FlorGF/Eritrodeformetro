7afdebf1d0c9b7d1a7001e6c660b33517d770f80
Florencia <florgomezfava.97@gmail.com>
2023-02-02 18:57:05 -0300

    funciones 2

--- eritro.py
+++ eritro.py
@@ -9,17 +9,56 @@ root=tk.Tk()
 ser=serial.Serial() #falta definir donde esta el puerto
 root.title("Eritrodeformetro")
 root.config(width=400,height=300)
-#defino los botones
 
-#botones de ensayo
-label_ensayo=ttk.LabelFrame(root,text="Ensayos:")
-label_ensayo.grid(column=0,row=0)
-bot_carga=ttk.Button(label_ensayo,text="Carga")
-bot_carga.grid(column=0,row=1)
-bot_descarga=ttk.Button(label_ensayo,text="Descarga")
-bot_descarga.grid(column=0,row=2)
-bot_dinamico=ttk.Button(label_ensayo,text="Dinamico")
-bot_dinamico.grid(column=0,row=3)
+#FUNCIONES
+#esta funcion es generica para escribir por el puerto serie, manda los token
+def comunicaUSB(cod_token):
+    ser.write(cod_token)
+
+#funcionalidad del boton de parada
+def parada(): 
+    cod="M00000"
+    print(cod)
+    #comunicaUSB(cod)
+
+
+#funcionalidad del puerto
+def f_puerto(): 
+    flag_puerto=puerto.get()
+    if flag_puerto == 1: 
+        ser.open()
+    if flag_puerto == 0:
+        ser.close()
+
+
+#funcionalidad del boton de carga
+def e_carga():
+    vel=velocidad.get()
+    cod="EC"+vel+"00"
+   # comunicaUSB(cod)
+    print(cod)
+
+#funcionalidad del boton de descarga
+def e_descarga():
+    vel=velocidad.get()
+    cod="ED"+vel+"00"
+    #comunicaUSB(cod)
+    print(cod)
+
+
+def e_dinamico():
+    vel=velocidad.get()
+    f=frec.get()
+    if(f != 5):
+        cod="EO"+vel+f
+    else:
+        cod="EO"+vel+"0"+f 
+    
+    print(cod)
+    #comunicaUSB(cod)
+
+##########################################################################################
+#defino los botones
 
 #botones de motor
 bot_encender=ttk.Button(root,text="Encender motor",command=parada)
@@ -28,12 +67,12 @@ bot_apagar=ttk.Button(root,text="Apagar motor")
 bot_apagar.grid(column=1,row=1)
 
 #botones del puerto
-flag_puerto=tk.StringVar()
+puerto=tk.StringVar()
 label_puerto=ttk.LabelFrame(root,text="Puerto:")
 label_puerto.grid(column=1,row=2)
-abrir_puerto=ttk.Radiobutton(label_puerto,text="Abrir",value=1,variable=flag_puerto)
+abrir_puerto=ttk.Radiobutton(label_puerto,text="Abrir",value=1,variable=puerto)
 abrir_puerto.grid(column=1,row=3)
-cerrar_puerto=ttk.Radiobutton(label_puerto,text="Cerrar",value=0,variable=flag_puerto)
+cerrar_puerto=ttk.Radiobutton(label_puerto,text="Cerrar",value=0,variable=puerto)
 cerrar_puerto.grid(column=1,row=4)
 
 #botones de frecuencia
@@ -58,17 +97,19 @@ intermedia=ttk.Radiobutton(label_velocidad,text="35rpm",value=35,variable=veloci
 intermedia.grid(column=1,row=7)
 mayor=ttk.Radiobutton(label_velocidad,text="70rpm",value=70,variable=velocidad)
 mayor.grid(column=1,row=8)
+
+#botones de ensayo
+label_ensayo=ttk.LabelFrame(root,text="Ensayos:")
+label_ensayo.grid(column=0,row=0)
+bot_carga=ttk.Button(label_ensayo,text="Carga",command=e_carga)
+bot_carga.grid(column=0,row=1)
+bot_descarga=ttk.Button(label_ensayo,text="Descarga",command=e_descarga)
+bot_descarga.grid(column=0,row=2)
+bot_dinamico=ttk.Button(label_ensayo,text="Dinamico",command=e_dinamico)
+bot_dinamico.grid(column=0,row=3)
+
+
+
+
+
 root.mainloop()
-
-##############################################################
-#FUNCIONES 
-def parada(): 
-    ser.write("M00000")
-
-def puerto(): 
-    if flag_puerto.get() == 1: 
-        ser.open()
-    if flag_puerto.get() == 0:
-        ser.close()
-
-
